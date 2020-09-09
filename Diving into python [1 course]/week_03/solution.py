ca47c3442b56f277c60f52351d91d92048f08931
Daler Rustamov <32430876+iamrustamov@users.noreply.github.com>
2020-09-09 04:26:54 +0300

    add files
    push week_03

---
+++ Diving into python [1 course]/week_03/solution.py
@@ -0,0 +1,10 @@
+class FileReader:
+    def __init__(self, path):
+        self.path = path
+
+    def read(self):
+        try:
+            with open(self.path, "r") as f:
+                return f.read()
+        except FileNotFoundError:
+            return ''
